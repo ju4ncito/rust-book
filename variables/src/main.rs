1e3b55187056736be1ab4cd858903bd373a75d99
juan <villarrealjm2@gmail.com>
2024-04-24 09:56:51 -0300

    ch3-03

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
+
+// Doing the check of the value the user inputs before accesing the memory provides safety.
+
+// re-asigning value with mut
 // fn main() {
 //     let mut x = 5;
 //     println!("The value of x is: {x}");
@@ -5,15 +31,45 @@
 //     println!("The value of x is: {x}");
 // }
 
-fn main() {
-    let x = 5;
+// Re-asigning value using shadowing and scopes
+// fn main() {
+//     let x = 5;
 
-    let x = x + 1;
+//     let x = x + 1;
 
-    {
-        let x: i32 = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+//     {
+//         let x: i32 = x * 2;
+//         println!("The value of x in the inner scope is: {x}");
+//     }
 
-    println!("The value of x is: {x}");
-}
+//     println!("The value of x is: {x}");
+// }
+
+// tuples
+
+// fn main() {
+//     let tup = (500, 6.4, 1);
+
+//     let (x, y, z) = tup;
+
+//     println!("The value of y is: {y}");
+// }
+
+// fn main() {
+//     let x: (i32, f64, u8) = (500, 6.4, 1);
+
+//     let five_hundred = x.0;
+
+//     let six_point_four = x.1;
+
+//     let one = x.2;
+// }
+
+// Arrays
+
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+
+//     let first = a[0];
+//     let second = a[1];
+// }
